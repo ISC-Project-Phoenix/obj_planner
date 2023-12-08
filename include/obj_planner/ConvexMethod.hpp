c261c7a2fe178217011f99e0dff2c8e2944e9290
Nathan Vargo <nvargo@umich.edu>
2023-12-08 12:16:51 -0500

    Implementation of the below methods of ConvexMethod
    Note: unable to test for some reason, even on clean branch
    
    get_detections_vector()
    get_convex_hull()
    determine_scenario()

--- include/obj_planner/ConvexMethod.hpp
+++ include/obj_planner/ConvexMethod.hpp
@@ -4,7 +4,7 @@
 #include "opencv2/core/types.hpp"
 
 struct ConvexMethodParams {
-    float turn_threshold{1.0F};               // meters
+    double turn_threshold{1.0F};              // meters
     float end_segment_angle_threshold{2.2F};  // radians
     float cluster_threshold{2.0F};            // meters
 };
@@ -15,27 +15,27 @@ enum class Scenario : std::uint8_t { kStraight = 0, kLeft = 1U, kRight = 2U };
 class IScenarioClassifier {
 public:
     /// Classifies detections into right and left sides of the track. If only one side is visible, return none.
-    virtual LeftRightResults classify(const std::vector<cv::Point>& convex_hull,
-                                      const std::vector<cv::Point>& detections_2d) = 0;
+    virtual LeftRightResults classify(const std::vector<cv::Point2d>& convex_hull,
+                                      const std::vector<cv::Point2d>& detections_2d) = 0;
     virtual ~IScenarioClassifier() = default;
 };
 
 class LeftScenarioClassifier : public IScenarioClassifier {
 public:
-    LeftRightResults classify(const std::vector<cv::Point>& convex_hull,
-                              const std::vector<cv::Point>& detections_2d) override;
+    LeftRightResults classify(const std::vector<cv::Point2d>& convex_hull,
+                              const std::vector<cv::Point2d>& detections_2d) override;
 };
 
 class RightScenarioClassifier : public IScenarioClassifier {
 public:
-    LeftRightResults classify(const std::vector<cv::Point>& convex_hull,
-                              const std::vector<cv::Point>& detections_2d) override;
+    LeftRightResults classify(const std::vector<cv::Point2d>& convex_hull,
+                              const std::vector<cv::Point2d>& detections_2d) override;
 };
 
 class StraightScenarioClassifier : public IScenarioClassifier {
 public:
-    LeftRightResults classify(const std::vector<cv::Point>& convex_hull,
-                              const std::vector<cv::Point>& detections_2d) override;
+    LeftRightResults classify(const std::vector<cv::Point2d>& convex_hull,
+                              const std::vector<cv::Point2d>& detections_2d) override;
 };
 
 /// ConvexMethod performs left right classification on an array of 2d points
@@ -52,16 +52,16 @@ public:
 
 private:
     /// Set detections_2d
-    std::vector<cv::Point> get_detections_vector(const geometry_msgs::msg::PoseArray& detections_array);
+    std::vector<cv::Point2d> get_detections_vector(const geometry_msgs::msg::PoseArray& detections_array);
 
-    /// Calculate and return the convex hull of a vector of 2d points
-    std::vector<cv::Point> get_convex_hull();
+    /// Calculate and return the counter clock-wise convex hull of a vector of 2d points
+    std::vector<cv::Point2d> get_convex_hull(const std::vector<cv::Point2d>& detections_2d);
 
     /// Check area of convex hull to determine if using this algo is valid
     bool is_convex_hull_valid();
 
     /// Determine if detections indicate we are turning left, right, or staying straight
-    Scenario determine_scenario();
+    Scenario determine_scenario(const std::vector<cv::Point2d>& detections_2d);
 
     ConvexMethodParams params{};
 
--- src/ConvexMethod.cpp
+++ src/ConvexMethod.cpp
@@ -1,12 +1,16 @@
 #include "obj_planner/ConvexMethod.hpp"
 
+#include <numeric>
+
+#include "opencv2/imgproc.hpp"
+
 std::optional<LeftRightResults> ConvexMethod::classify(const geometry_msgs::msg::PoseArray& detections_array) {
     std::optional<LeftRightResults> classification{std::nullopt};
-    auto detections_2d = get_detections_vector(detections_array);
-    const auto convex_hull = get_convex_hull();
+    const auto detections_2d = get_detections_vector(detections_array);
+    const auto convex_hull = get_convex_hull(detections_2d);
 
     if (is_convex_hull_valid()) {
-        const auto scenario = determine_scenario();
+        const auto scenario = determine_scenario(detections_2d);
 
         if (scenario == Scenario::kStraight) {
             scenario_classifier = straight_scenario_classifier;
@@ -22,45 +26,61 @@ std::optional<LeftRightResults> ConvexMethod::classify(const geometry_msgs::msg:
     return classification;
 }
 
-std::vector<cv::Point> ConvexMethod::get_detections_vector(const geometry_msgs::msg::PoseArray& detections_2d) {
-    //TODO impl
-    (void)detections_2d;
-    return {};
+std::vector<cv::Point2d> ConvexMethod::get_detections_vector(const geometry_msgs::msg::PoseArray& detections_array) {
+    std::vector<cv::Point2d> detections_2d{};
+    for (const auto& pose : detections_array.poses) {
+        detections_2d.emplace_back(pose.position.x, pose.position.y);
+    }
+    return detections_2d;
 }
 
-std::vector<cv::Point> ConvexMethod::get_convex_hull() {
-    //TODO impl
-    return {};
+std::vector<cv::Point2d> ConvexMethod::get_convex_hull(const std::vector<cv::Point2d>& detections_2d) {
+    std::vector<cv::Point2d> convex_hull{};
+    cv::convexHull(detections_2d, convex_hull, false);
+    return convex_hull;
 }
 
 bool ConvexMethod::is_convex_hull_valid() {
-    //TODO impl
-    return false;
+    //TODO For now set to true, add some area based impl.
+    return true;
 }
 
-Scenario ConvexMethod::determine_scenario() {
+Scenario ConvexMethod::determine_scenario(const std::vector<cv::Point2d>& detections_2d) {
     //TODO impl
-    return Scenario::kStraight;
+    Scenario scenario{Scenario::kStraight};
+
+    if (detections_2d.size() > 0) {
+        const double sum_in_y =
+            std::accumulate(std::begin(detections_2d), std::end(detections_2d), 0.,
+                            [](const auto& accumulation, const auto& point) { return accumulation + point.y; });
+        const double center_of_mass_in_y{sum_in_y / detections_2d.size()};
+        if (center_of_mass_in_y > params.turn_threshold) {
+            scenario = Scenario::kLeft;
+        } else if (center_of_mass_in_y < -params.turn_threshold) {
+            scenario = Scenario::kRight;
+        }
+    }
+    return scenario;
 }
 
-LeftRightResults StraightScenarioClassifier::classify(const std::vector<cv::Point>& convex_hull,
-                                                      const std::vector<cv::Point>& detections_2d) {
+LeftRightResults StraightScenarioClassifier::classify(const std::vector<cv::Point2d>& convex_hull,
+                                                      const std::vector<cv::Point2d>& detections_2d) {
     //TODO impl
     (void)convex_hull;
     (void)detections_2d;
     return {};
 }
 
-LeftRightResults LeftScenarioClassifier::classify(const std::vector<cv::Point>& convex_hull,
-                                                  const std::vector<cv::Point>& detections_2d) {
+LeftRightResults LeftScenarioClassifier::classify(const std::vector<cv::Point2d>& convex_hull,
+                                                  const std::vector<cv::Point2d>& detections_2d) {
     //TODO impl
     (void)convex_hull;
     (void)detections_2d;
     return {};
 }
 
-LeftRightResults RightScenarioClassifier::classify(const std::vector<cv::Point>& convex_hull,
-                                                   const std::vector<cv::Point>& detections_2d) {
+LeftRightResults RightScenarioClassifier::classify(const std::vector<cv::Point2d>& convex_hull,
+                                                   const std::vector<cv::Point2d>& detections_2d) {
     //TODO impl
     (void)convex_hull;
     (void)detections_2d;
